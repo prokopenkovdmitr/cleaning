76ceac29a3b8d2bceebed02cc33329f478e57e5b
prokopenkovdmitr <prokopenkovdmitr@gmail.com>
2022-06-15 16:28:20 +0300

    second commit

--- cleaning/settings.py
+++ cleaning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-prdi1#c592-n3&erar^g#r)!3#hl#-rpr(7kjiu-*p891tc1yk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,11 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
 #    '/var/www/static/',
-]
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
