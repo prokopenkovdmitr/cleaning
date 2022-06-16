6017f12f832125d79b6b6131e44b94f26e618b8c
prokopenkovdmitr <prokopenkovdmitr@gmail.com>
2022-06-16 11:23:39 +0300

    3 commit.

--- cleaning/settings.py
+++ cleaning/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,17 +119,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 #STATIC_ROOT = "/home/dmprok.pythonanywhere.com/cleaning/main/static/"
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    #BASE_DIR / "static",
-    '/home/dmprok.pythonanywhere.com/cleaning/main/static/',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+
+#    '/home/dmprok.pythonanywhere.com/cleaning/main/static/',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
